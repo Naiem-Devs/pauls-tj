827e98aa5c8ae3eacedbcc90539734cbc9e95803
Developer-Nasim <ajnasim72@gmail.coom>
2021-12-20 02:14:19 +0600

    video issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,42 @@
 (function($) {
   "use strict";
 
-  // scroll Down
-  function ScrollDown() {
-    setTimeout(() => {
-        let scrollDown = document.querySelector('.package-area').offsetTop;
-        window.scroll({
-            top: scrollDown,
-            left: 0,
-            behavior: 'smooth'
-        }); 
-    }, 100);
-  }
-
-
-
-  // View more
-  $('.more').click(function () {
-    $('.rl_blk').toggleClass('show')
-  })
-  
-
   // Load the page and then do rest of the work 
   window.addEventListener('load', (e) => {
+    // scroll Down
+    function ScrollDown() {
+        setTimeout(() => {
+            let scrollDown = document.querySelector('.package-area').offsetTop;
+            window.scroll({
+                top: scrollDown,
+                left: 0,
+                behavior: 'smooth'
+            }); 
+        }, 100);
+    }
 
+    // Video Functions
+    function VideoFunction(device,second) {
+        const timeInterVal = setInterval(() => {
+            let getVideo = document.querySelector(device+' video');
+            if (getVideo.muted !== true) {
+                console.log(getVideo.currentTime)
+                if (getVideo.currentTime >= second) { // if the video current time is equal or over 10 second 
+                    localStorage.setItem('is_visited', 'yes') // set the value as yes on localStorage of the browser
+                    document.querySelector('body').classList.add('visited') // and add a class called "Visited" to do other tasks
+                    console.log('working')
+                    clearInterval(timeInterVal) // and stop this interval
+                    ScrollDown()
+                }
+            }
+        }, 2000);
+    }
+ 
+    // View more
+    $('.more').click(function () {
+        $('.rl_blk').toggleClass('show')
+    })
+   
     // Load Pupup on page load
     $('#promoModal').modal('show');
 
@@ -32,52 +45,18 @@
     if (this_user_visited == 'yes') {
         document.querySelector('body').classList.add('visited')
     }else{ 
+          
+        let mediaQue = window.matchMedia("(max-width:844px)");
+        if (mediaQue.matches) {
+            // ====== For Mobile ==== 
+            VideoFunction('.mobile_view ',10)
+        }else{
+            // ====== For Desktop ==== 
+            VideoFunction('.desktop_view ',10)
+        }
         
-        // ====== For Desktop ==== 
-        function DesktopVideoFunc() { 
-            // Find the clickable position 
-            let toClick_desktop = document.querySelector('.desktop_view div#bmpui-id-8');
-            if (toClick_desktop) {  // if that is avileable
-                toClick_desktop.addEventListener('click', (e) => { // if clicked on that
-                    let getVideo = document.querySelector('.desktop_view video'); // finding the video
-                    const timeInterVal = setInterval(() => {// checking in each second the current time of the video
-                        if (getVideo.currentTime >= 10) { // if the video current time is equal or over 10 second
-                            console.log('it is over 10 second')
-                            localStorage.setItem('is_visited', 'yes') // set the value as yes on localStorage of the browser
-                            document.querySelector('body').classList.add('visited') // and add a class called "Visited" to do other tasks
-                            clearInterval(timeInterVal) // and stop this interval
-                            ScrollDown()
-                        }
-                    }, 1000);
-                })
-            }
-        }
-        DesktopVideoFunc()
- 
-        // ====== For Mobile ====
-        function MobileVideoFunc() { 
-            // Find the clickable position 
-            let toClick_mobile = document.querySelector('.mobile_view div#bmpui-id-7');
-            if (toClick_mobile) {  // if that is avileable
-                toClick_mobile.addEventListener('click', (e) => { // if clicked on that
-                console.log(toClick_mobile)
-                    let getVideo = document.querySelector('.mobile_view video'); // finding the video
-                    const timeInterVal = setInterval(() => {// checking in each second the current time of the video
-                        if (getVideo.currentTime >= 10) { // if the video current time is equal or over 10 second
-                            console.log('it is over 10 second')
-                            localStorage.setItem('is_visited', 'yes') // set the value as yes on localStorage of the browser
-                            document.querySelector('body').classList.add('visited') // and add a class called "Visited" to do other tasks
-                            clearInterval(timeInterVal) // and stop this interval
-                            ScrollDown()
-                        }
-                    }, 1000);
-                })
-            }
-        }
-        MobileVideoFunc()
     }
-
-    
+ 
 })
   
  
